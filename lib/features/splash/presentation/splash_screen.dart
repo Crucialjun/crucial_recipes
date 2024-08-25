cbfa507955e357b2bc6980252321898086c04bb7
Nicholas Otieno <nich.otieno@gmail.com>
2024-08-26 01:22:44 +0300

    chore: Update app.dart to import SplashScreen and change theme color

--- lib/features/splash/presentation/splash_screen.dart
+++ lib/features/splash/presentation/splash_screen.dart
@@ -1,3 +1,5 @@
+import 'package:crucial_recipes/constants/app_assets.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -5,13 +7,17 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text('Splash Screen',
-              style: TextStyle(fontSize: 24, color: Colors.deepOrange)),
+    return Scaffold(
+        body: Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ExtendedImage.asset(
+          AppAssets.logo,
+          width: 200,
+          height: 200,
         ),
-      ),
-    );
+      ],
+    ));
   }
 }
